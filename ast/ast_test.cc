7da076a3d73eb9dc0927752936a050946576afb3
Betai Koffi <2094883+betai@users.noreply.github.com>
2025-02-24 12:57:19 -0800

    begin ast (empty)

---
+++ ast/ast_test.cc
@@ -0,0 +1,15 @@
+#include "ast.h"
+
+#include "gtest/gtest.h"
+
+namespace simp {
+namespace {
+class AstTest : public ::testing::Test {
+ protected:
+  AstTest() {}
+  ~AstTest() override {}
+  void SetUp() override {}
+};
+
+}  // namespace
+}  // namespace simp
