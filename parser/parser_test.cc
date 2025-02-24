6664ffd12facae898e3a7e9f1b12a2e793b6fa38
Betai Koffi <2094883+betai@users.noreply.github.com>
2025-02-24 00:33:42 -0800

    begin parser (empty)

---
+++ parser/parser_test.cc
@@ -0,0 +1,10 @@
+#include "parser.h"
+
+#include "gtest/gtest.h"
+
+class ParserTest : public ::testing::Test {
+ protected:
+  ParserTest() {}
+  ~ParserTest() override {}
+  void SetUp() override {}
+};
