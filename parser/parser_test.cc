8bd6b5648fe02c041ca1bc45f575239af0b7d33c
Betai Koffi <2094883+betai@users.noreply.github.com>
2025-02-24 00:57:00 -0800

    clean up parser test

--- parser/parser_test.cc
+++ parser/parser_test.cc
@@ -2,9 +2,14 @@
 
 #include "gtest/gtest.h"
 
+namespace simp {
+namespace {
 class ParserTest : public ::testing::Test {
  protected:
   ParserTest() {}
   ~ParserTest() override {}
   void SetUp() override {}
 };
+
+}  // namespace
+}  // namespace simp
