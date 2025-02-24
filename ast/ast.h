5cb0b1349c2ecbffa4f64b3731dc46dbde55be99
Betai Koffi <2094883+betai@users.noreply.github.com>
2025-02-24 14:25:59 -0800

    add IntExpression and IfExpression

--- ast/ast.h
+++ ast/ast.h
@@ -1,1 +1,92 @@
+#pragma once
+#include <iostream>
+#include <memory>
+
 #include "tokens/tokens.h"
+
+namespace simp {
+enum class ExpressionType {
+  INTEGER,
+  IF,
+  // IN,
+  // LET,
+  // PLUS,
+  // MINUS,
+  // TIMES,
+  // LESS_THAN,
+  // RECUR,
+  // LOOP,
+  // AND,
+  // END,
+};
+class Expression {
+ public:
+  Expression(ExpressionType type) : type_(type) {}
+  virtual int eval() = 0;
+  virtual void print() = 0;
+  virtual std::string to_string() = 0;
+  ExpressionType type() { return type_; }
+
+ private:
+  ExpressionType type_;
+};
+
+class IntExpression : public Expression {
+ public:
+  IntExpression(int value)
+      : Expression(ExpressionType::INTEGER), value_(value) {}
+  int eval() override { return value_; }
+  void print() override {
+    std::cout << "Int expression with value:" << value_ << std::endl;
+  }
+  std::string to_string() override { return std::to_string(value_); }
+
+ private:
+  int value_;
+};
+class IfExpression : public Expression {
+ public:
+  IfExpression(std::unique_ptr<KeywordToken> if_token,
+               std::unique_ptr<Expression> condition,
+               std::unique_ptr<KeywordToken> then_token,
+               std::unique_ptr<Expression> consequent,
+               std::unique_ptr<KeywordToken> else_token,
+               std::unique_ptr<Expression> alternative)
+      : Expression(ExpressionType::IF),
+        if_token_(std::move(if_token)),
+        condition_(std::move(condition)),
+        then_token_(std::move(then_token)),
+        consequent_(std::move(consequent)),
+        else_token_(std::move(else_token)),
+        alternative_(std::move(alternative)) {}
+
+  std::unique_ptr<KeywordToken>& if_token() { return if_token_; }
+  std::unique_ptr<KeywordToken>& then_token() { return then_token_; }
+  std::unique_ptr<KeywordToken>& else_token() { return else_token_; }
+  std::unique_ptr<Expression>& condition() { return condition_; }
+  std::unique_ptr<Expression>& consequent() { return consequent_; }
+  std::unique_ptr<Expression>& alternative() { return alternative_; }
+
+  int eval() override {
+    if (condition_->eval()) {
+      return consequent_->eval();
+    }
+    return alternative_->eval();
+  }
+  void print() override { std::cout << to_string() << std::endl; }
+  std::string to_string() override {
+    return "If\ncondition:" + condition_->to_string() +
+           "\nthen:" + consequent_->to_string() +
+           "\nelse:" + alternative_->to_string();
+  }
+
+ private:
+  std::unique_ptr<KeywordToken> if_token_;
+  std::unique_ptr<KeywordToken> then_token_;
+  std::unique_ptr<KeywordToken> else_token_;
+  std::unique_ptr<Expression> condition_;
+  std::unique_ptr<Expression> consequent_;
+  std::unique_ptr<Expression> alternative_;
+};
+
+}  // namespace simp
--- ast/ast_test.cc
+++ ast/ast_test.cc
@@ -11,5 +11,33 @@ class AstTest : public ::testing::Test {
   void SetUp() override {}
 };
 
+TEST_F(AstTest, CreatesIntExpression) {
+  IntExpression int_expr(42);
+  EXPECT_EQ(int_expr.eval(), 42);
+  EXPECT_EQ(int_expr.to_string(), "42");
+}
+
+TEST_F(AstTest, CreatesIfExpression) {
+  std::unique_ptr<KeywordToken> if_token =
+      std::make_unique<KeywordToken>("if", 0, 0, "test");
+  std::unique_ptr<KeywordToken> then_token =
+      std::make_unique<KeywordToken>("then", 0, 0, "test");
+  std::unique_ptr<KeywordToken> else_token =
+      std::make_unique<KeywordToken>("then", 0, 0, "test");
+  std::unique_ptr<IntExpression> condition = std::make_unique<IntExpression>(1);
+  std::unique_ptr<IntExpression> consequent_expr =
+      std::make_unique<IntExpression>(2);
+  std::unique_ptr<IntExpression> alternative_expr =
+      std::make_unique<IntExpression>(3);
+  IfExpression if_expr(std::move(if_token), std::move(condition),
+                       std::move(then_token), std::move(consequent_expr),
+                       std::move(else_token), std::move(alternative_expr));
+  EXPECT_EQ(if_expr.to_string(), "If\ncondition:1\nthen:2\nelse:3");
+  EXPECT_EQ(if_expr.condition()->eval(), 1);
+  EXPECT_EQ(if_expr.consequent()->eval(), 2);
+  EXPECT_EQ(if_expr.alternative()->eval(), 3);
+  EXPECT_EQ(if_expr.eval(), 2);
+}
+
 }  // namespace
 }  // namespace simp
